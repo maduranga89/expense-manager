192cbe82c272bf9bad9f8bd65a54d5a11179cc99
maduranga89 <maduranga89@gmail.com>
2021-12-05 21:39:16 +0530

    Started Java Implementation

--- app/src/main/java/com/londontec/expensemanager/RegistrationActivity.java
+++ app/src/main/java/com/londontec/expensemanager/RegistrationActivity.java
@@ -2,13 +2,59 @@ package com.londontec.expensemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText mEmail;
+    private EditText mPass;
+    private Button btnReg;
+    private TextView mSignin;
+
+    //Firebase Integration
+    private FireBaseAuth
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        registration();
+    }
+
+    private void registration() {
+        mEmail = findViewById(R.id.email_reg);
+        mPass = findViewById(R.id.password_reg);
+        btnReg = findViewById(R.id.btn_reg);
+        mSignin = findViewById(R.id.sign_in_here);
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString();
+                String pass = mPass.getText().toString();
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email Required...!");
+                    return;
+                }
+                if(TextUtils.isEmpty(pass)){
+                    mPass.setError("Password Is Required...!");
+                    return;
+                }
+            }
+        });
+
+        mSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
     }
 }
